61cba8268723f1098c128b75832e85d9fe32a687
alan-g04 <nutkaalan19@gmail.com>
2026-05-06 19:33:37 +0100

    Added uncompleted Exercise 6

---
+++ 6-Noughts_and_crosses/6-Noughts_and_crosses.hs
@@ -0,0 +1,18 @@
+
+import System.Random
+
+{-
+import Data.Time.Clock
+
+randomIO :: IO Int
+randomIO = do
+  t <- getCurrentTime
+  (return . floor . (*1000) . toRational . utctDayTime) t
+-}
+
+
+------------------------- Exercise 1
+
+main :: IO ()
+main = undefined
+
