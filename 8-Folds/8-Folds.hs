63fdaa6c6e018aa34635c5822ba97f66d8668b80
alan-g04 <nutkaalan19@gmail.com>
2026-05-06 19:34:00 +0100

    Added uncompleted Exercise 8

---
+++ 8-Folds/8-Folds.hs
@@ -0,0 +1,62 @@
+
+
+------------------------- Exercise 1
+
+allTrue :: [Bool] -> Bool
+allTrue = undefined
+
+someTrue :: [Bool] -> Bool
+someTrue = undefined
+
+largest :: Ord a => [a] -> a
+largest = undefined
+
+smallest :: Ord a => [a] -> a
+smallest = undefined
+
+every :: (a -> Bool) -> [a] -> Bool
+every = undefined
+
+some :: (a -> Bool) -> [a] -> Bool
+some = undefined
+
+select :: (a -> Bool) -> [a] -> [a]
+select = undefined
+
+member :: Eq a => a -> [a] -> Bool
+member = undefined
+
+find :: Eq a => a -> [(a,b)] -> Maybe b
+find = undefined
+
+------------------------- Exercise 2
+
+
+evenLength :: [a] -> Bool
+evenLength = undefined
+
+count :: (a -> Bool) -> [a] -> Int
+count = undefined
+
+successive :: Char -> String -> Int
+successive = undefined
+
+
+------------------------- Exercise 3
+
+{-
+
+Category 1 (foldl "just works"):
+
+Category 2 (a "flip" is needed, but same result):
+
+Category 3 (different result even with "flip"):
+
+-}
+
+------------------------- Exercise 4
+
+------------------------- Exercise 5
+
+
+
